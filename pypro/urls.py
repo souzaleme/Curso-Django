51baf7cc7a44ecbfb3f91409ce7b1ac630da7808
souzaleme <souzaleme@yahoo.com.br>
2022-04-13 19:56:11 -0300

    Atualizando para rodar o 'Olá Django'

--- pypro/urls.py
+++ pypro/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from views import home
 
+app_name = 'base'
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name = 'home'),
 ]
